5cdb84111b9a137b4a8f2b995b295adab2657d49
Joel Gustafson <joelg@mit.edu>
2017-09-22 13:04:08 -0400

    clouds made of ambien

--- makefile
+++ makefile
@@ -1,3 +1,4 @@
+# replace "-ltfhe-spqlios-fma" with the right version of TFHE from tfhe_utils.py
 all: build
 build:
 	gcc -shared -Wl,-install_name,tfhe_io.so -L/usr/local/lib -ltfhe-spqlios-fma -o tfhe_io.so -fPIC tfhe_io.c
--- tfhe_io.c
+++ tfhe_io.c
@@ -2,7 +2,9 @@
 #include <tfhe/tfhe.h>
 #include <tfhe/tfhe_io.h>
 
+
 /** Gate parameters */
+
 void export_gate_params(const char* filename, const TFheGateBootstrappingParameterSet* params) {
     FILE* file = fopen(filename, "w");
     export_tfheGateBootstrappingParameterSet_toFile(file, params);
@@ -16,7 +18,9 @@ TFheGateBootstrappingParameterSet* import_gate_params(const char* filename) {
     return params;
 }
 
+
 /** Secret keysets */
+
 void export_secret_keyset(const char* filename, const TFheGateBootstrappingSecretKeySet* params) {
     FILE* file = fopen(filename, "w");
     export_tfheGateBootstrappingSecretKeySet_toFile(file, params);
@@ -30,7 +34,9 @@ TFheGateBootstrappingSecretKeySet* import_secret_keyset(const char* filename) {
     return keyset;
 }
 
+
 /** Cloud keysets */
+
 void export_cloud_keyset(const char* filename, const TFheGateBootstrappingCloudKeySet* params) {
     FILE* file = fopen(filename, "w");
     export_tfheGateBootstrappingCloudKeySet_toFile(file, params);
@@ -44,7 +50,9 @@ TFheGateBootstrappingCloudKeySet* import_cloud_keyset(const char* filename) {
     return keyset;
 }
 
+
 /** Ciphertexts */
+
 void export_ciphertext (const char* filename, const LweSample* sample, const TFheGateBootstrappingParameterSet* params) {
     FILE* file = fopen(filename, "w");
     export_gate_bootstrapping_ciphertext_toFile(file, sample, params);
